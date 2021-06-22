1f25a4da7ed57da5c255e9fe89a793e15fca84b9
kquinn1998 <kevquinn321@gmail.com>
2021-06-22 16:55:20 +0100

    Initial Commit

---
+++ AmbeeWeatherAPIFetcher/APIManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmbeeWeatherAPIFetcher
+{
+    public class APIManager
+    {
+        private string _apiKey;
+        public APIManager()
+        {
+
+        }
+    }
+}
---
+++ AmbeeWeatherAPIFetcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AmbeeWeatherAPIFetcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            APIManager api = new APIManager();
+        }
+    }
+}
