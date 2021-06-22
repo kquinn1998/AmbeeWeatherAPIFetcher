84f95944f4729021e10ba608204ede3c635b75a3
kquinn1998 <kevquinn321@gmail.com>
2021-06-22 21:25:49 +0100

    Work on APIManager

--- AmbeeWeatherAPIFetcher/APIManager.cs
+++ AmbeeWeatherAPIFetcher/APIManager.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+using AmbeeWeatherAPIFetcher.Entities;
+using Newtonsoft.Json;
 
 namespace AmbeeWeatherAPIFetcher
 {
@@ -11,7 +15,34 @@ namespace AmbeeWeatherAPIFetcher
         private string _apiKey;
         public APIManager()
         {
+            setApiKey();
+        }
 
+        private void setApiKey()
+        {
+
+            using (StreamReader r = new StreamReader("secrets.json"))
+            {
+                try
+                {
+                    string json = r.ReadToEnd();
+                    Secrets input = JsonConvert.DeserializeObject<Secrets>(json.ToString());
+                    if(string.IsNullOrEmpty(input.api_key))
+                    {
+                        throw new NullReferenceException("Error... could not retrieve API key.");
+                    }
+                    else
+                    {
+                        _apiKey = input.api_key;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    throw;
+                }
+
+            }
         }
     }
 }
---
+++ AmbeeWeatherAPIFetcher/Entities/Secrets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmbeeWeatherAPIFetcher.Entities
+{
+    public class Secrets
+    {
+        public string api_key { set; get; }
+    }
+}
