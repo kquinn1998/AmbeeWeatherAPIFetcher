a5c49976e7bf8dbf0d16ae54ee78ae9448a73b8a
kquinn1998 <kevquinn321@gmail.com>
2021-06-23 18:29:08 +0100

    Initial Connection made to geocoding api

---
+++ AmbeeWeatherAPIFetcher/GeocodingAPIManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmbeeWeatherAPIFetcher
+{
+    public class GeocodingAPIManager
+    {
+        private APIManager _apiConfig;
+        public GeocodingAPIManager(APIManager apiConfig)
+        {
+            _apiConfig = apiConfig;
+            testConnection();
+        }
+        private void testConnection()
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create($"{_apiConfig._geocodingApiAddress}json?address=Ireland");
+            request.AllowAutoRedirect = false; // find out if this site is up and don't follow a redirector
+            request.Method = "HEAD";
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new WebException($"Error with connecting to API.... {response.StatusCode} : {response.StatusDescription}");
+                }
+                else
+                {
+                    Console.WriteLine("Connection To Geocoding API Successful");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
